2f6e0c0c1935395e5da599e2fad1d9cc5ccf451a
nazimrasheed <mr.rasheed1991@gmail.com>
2021-04-02 23:59:24 +0100

    added display time

--- Main.js/script.js
+++ Main.js/script.js
@@ -1,1 +1,10 @@
+var timeDisplayEl = $("#time-display");
 
+// handle displaying the time
+function displayTime() {
+  var rightNow = moment().format("MMM DD, YYYY");
+  timeDisplayEl.text(rightNow);
+  console.log(moment());
+}
+
+displayTime();
