092e1737a21fceff5d71d1e6722dcfd7bb1badcc
nazimrasheed <mr.rasheed1991@gmail.com>
2021-04-03 21:45:27 +0100

    added current date and time with setinterval

--- Main.js/script.js
+++ Main.js/script.js
@@ -8,7 +8,10 @@
 // };
 
 const getCurrentDate = () => {
-  const currentDate = moment().format("dddd Do MMMM");
-  $("#currentDay").text(currentDate);
-  return currentDate;
+  const currentDate = moment().format("dddd, Do of MMMM YYYY, h:mm:ss");
+  $("#currentDay").text(moment().format("dddd, Do of MMMM YYYY, h:mm:ss"););
 };
+
+setInterval(getCurrentDate, 1000);
+
+getCurrentDate();
