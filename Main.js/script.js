75b93a4eb9685f3550e3796d219e91d1b3ded08b
nazimrasheed <mr.rasheed1991@gmail.com>
2021-04-04 19:49:07 +0100

    added javascript function for row

--- Main.js/script.js
+++ Main.js/script.js
@@ -1,17 +1,28 @@
-// var timeDisplayEl = $("#time-display");
-
-// handle displaying the time
-// const currentTime = () => {
-//   const rightNow = moment().format("MMM DD, YYYY");
-//   $("#time-display").text(rightNow);
-//   return rightNow;
-// };
-
 const getCurrentDate = () => {
   const currentDate = moment().format("dddd, Do of MMMM YYYY, h:mm:ss");
-  $("#currentDay").text(moment().format("dddd, Do of MMMM YYYY, h:mm:ss"););
+  $("#currentDay").text(currentDate);
 };
 
+// using setinterval for the clock second to repeat everysecond
 setInterval(getCurrentDate, 1000);
+const renderTextArea = () => {
+  const plannerEvents = JSON.parse(localStorage.getItem("plannerEvents"));
+  if (plannerEvents !== null) {
+    const getTheCurrentHour = moment().hour();
+    const timeBlockArray = $(".container .row");
+    function callback() {
+      const textArea = $(this).find("textarea");
+      const time = parseInt($(this).data("time"));
+      if (time === getTheCurrentHour) {
+        textArea.removeClass("past").addClass("present");
+      }
+      if (time > getTheCurrentHour) {
+        textArea.removeClass("present").addClass("future");
+      }
+    }
+    timeBlockArray.each(callback);
+  }
+};
+const onload = () => {};
 
-getCurrentDate();
+$(document).ready(onload);
