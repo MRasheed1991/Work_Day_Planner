b1854c905642fd2f47031ec9a00bb0d784175b68
nazimrasheed <mr.rasheed1991@gmail.com>
2021-04-04 22:08:24 +0100

    added icon on the save button

--- Main.js/script.js
+++ Main.js/script.js
@@ -3,8 +3,13 @@ const getCurrentDate = () => {
   $("#currentDay").text(currentDate);
 };
 
+const colorCodeBlock = () => {
+  text.each(setcolor);
+};
+
 // using setinterval for the clock second to repeat everysecond
 setInterval(getCurrentDate, 1000);
+
 const renderTextArea = () => {
   const plannerEvents = JSON.parse(localStorage.getItem("plannerEvents"));
   if (plannerEvents !== null) {
@@ -15,16 +20,41 @@ const renderTextArea = () => {
       const time = parseInt($(this).data("time"));
       if (time === getTheCurrentHour) {
         textArea.removeClass("past").addClass("present");
+      } else if (time > getTheCurrentHour) {
+        textArea.removeClass("past").addClass("future");
       }
-      if (time > getTheCurrentHour) {
-        textArea.removeClass("present").addClass("future");
-      }
+      const plannedEvent = plannerEvents[time];
+      $(this).find("textarea").text(plannedEvent);
     }
     timeBlockArray.each(callback);
+  } else {
+    localStorage.setItem("plannerEvents", JSON.stringify({}));
   }
 };
+
+const onclick = function (event) {
+  const plannerEvents = JSON.parse(localStorage.getItem("plannerEvents"));
+  const target = $(event.target);
+  //this stops event bubbling
+  if (target.is("button")) {
+    const key = target.attr("id");
+    const value = target.parent().find("textarea").val();
+
+    //new object that stores user text
+    const filledScheduleObject = {
+      ...plannerEvents,
+      [key]: value,
+    };
+
+    //this pushes the user text in local storage
+    localStorage.setItem("plannerEvents", JSON.stringify(filledScheduleObject));
+  }
+};
+
 const onload = () => {
-  renderTextArea();
+  getCurrentDate();
+  colorCodeBlock();
+  $(".container").click(onClick);
 };
 
 $(document).ready(onload);
